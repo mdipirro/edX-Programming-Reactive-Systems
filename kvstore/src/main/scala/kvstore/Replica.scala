d2623cba822b973ff6e53566b3fcc20083a4ab12
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2020-03-03 11:06:48 +0100

    Implement Step 4
    This involves the interaction between a secondary replica
    and the Persistence actor.

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -1,10 +1,14 @@
 package kvstore
 
-import akka.actor.{ OneForOneStrategy, PoisonPill, Props, SupervisorStrategy, Terminated, ActorRef, Actor }
+import akka.actor.SupervisorStrategy.Restart
+import akka.actor.{Actor, ActorRef, Cancellable, OneForOneStrategy, PoisonPill, Props, SupervisorStrategy, Terminated}
 import kvstore.Arbiter._
-import akka.pattern.{ ask, pipe }
+import akka.pattern.{ask, pipe}
+
 import scala.concurrent.duration._
 import akka.util.Timeout
+
+import scala.language.postfixOps
 
 object Replica {
   sealed trait Operation {
@@ -29,16 +33,22 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   import Persistence._
   import context.dispatcher
 
+  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(-1, Duration.Inf) {
+    case _: PersistenceException => Restart
+  }
+
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
-  
+
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
 
+  private val persistence = context.actorOf(Persistence.props(false))
+  private var pendingAck = Option.empty[(ActorRef, Cancellable)]
 
   def receive = {
     case JoinedPrimary   => context.become(leader)
@@ -48,28 +58,38 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   /* TODO Behavior for  the leader role. */
   val leader: Receive = {
     case Insert(k, v, id) =>
-      kv = kv.updated(k, v)
+      kv = kv updated (k, v)
       sender ! OperationAck(id)
     case Remove(k, id) =>
-      kv = kv.removed(k)
+      kv = kv removed k
       sender ! OperationAck(id)
     case Get(k, id) => replyToLookup(k, id)
     case _ =>
   }
 
-  /* TODO Behavior for the replica role. */
   def replica(expectedSeq: Long): Receive = {
     case Get(k, id) => replyToLookup(k, id)
-    case Snapshot(k, ov, seq) =>
-      if (seq == expectedSeq) {
-        kv = ov.fold(kv removed k)(kv updated (k, _))
-        context become replica(expectedSeq + 1)
-        sender ! SnapshotAck(k, seq)
-      } else if (seq < expectedSeq) {
-        sender ! SnapshotAck(k, seq)
-        // seq + 1 is certainly <= expectedSeq, so no become() is necessary
-      }
-      // just ignore the message if seq > expectedSeq
+    case Insert(_, _, id) => sender ! OperationFailed(id)
+    case Remove(_, id) => sender ! OperationFailed(id)
+    case Snapshot(k, ov, seq) if seq == expectedSeq =>
+      kv = ov.fold(kv removed k)(kv updated (k, _))
+      val client = sender
+
+      pendingAck = Some(client -> context.system.scheduler.scheduleWithFixedDelay(
+        initialDelay = 0 millis,
+        delay = 100 millis,
+        receiver = persistence,
+        message = Persist(k, ov, seq)
+      ))
+    case Snapshot(k, ov, seq) if seq < expectedSeq =>
+      sender ! SnapshotAck(k, seq) // seq + 1 is certainly <= expectedSeq, so no become() is necessary
+    // just ignore the message if seq > expectedSeq
+    case Persisted(k, id) if id == expectedSeq => pendingAck foreach { case (client, timer) =>
+      pendingAck = None
+      context become replica(expectedSeq + 1L)
+      timer.cancel()
+      client ! SnapshotAck(k, id)
+    }
   }
 
   private def replyToLookup(key: String, opId: Long): Unit = sender ! GetResult(key, kv get key, opId)
